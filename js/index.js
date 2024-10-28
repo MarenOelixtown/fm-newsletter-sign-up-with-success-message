933030c0660420c9b4ea2cda664c7c2a68f2af49
Maren <marenehlers@gmx.net>
2024-10-28 15:47:54 +0100

    adding email-input-validation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 const signupForm = document.querySelector('[data-js="signup-form"]');
+const emailInput = document.querySelector('[data-js="email-input"]');
+const emailAlert = document.querySelector('[data-js="email-alert"]');
 const successDialog = document.querySelector('[data-js="success-dialog"]');
 const dialogEmail = document.querySelector('[data-js="dialog-email"]');
 const dismissButton = document.querySelector('[data-js="dismiss-button"]');
+
+emailInput.addEventListener("input", function () {
+  if (!emailInput.checkValidity()) {
+    emailAlert.classList.remove("hidden");
+    emailInput.classList.add("signup-form_email-input--invalid");
+  } else {
+    emailAlert.classList.add("hidden");
+    emailInput.classList.remove("signup-form_email-input--invalid");
+  }
+});
 
 signupForm.addEventListener("submit", (event) => {
   console.log("clicked");
@@ -9,8 +21,9 @@ signupForm.addEventListener("submit", (event) => {
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
   console.log(data);
-  event.target.reset();
+  dialogEmail.textContent = data.email;
   successDialog.showModal();
+  event.target.reset();
 });
 
 dismissButton.addEventListener("click", () => {
